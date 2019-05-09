a6ccbf25a039ba4aecbba40a38adba724810070a
Yangbin <yss920@163.com>
2019-05-09 11:53:59 +0000

    认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EmailVerified
+{
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        // 会话里闪存认证成功后的消息提醒
+        session()->flash('success', '邮箱验证成功 ^_^');
+    }
+}
