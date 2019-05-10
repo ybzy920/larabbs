420dacd677b02ffb5209ba4cfbf7a06b5b5aed00
Yangbin <yss920@163.com>
2019-05-10 10:12:48 +0000

    上传头像

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,7 +14,7 @@
 
       <div class="card-body">
 
-        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+        <form action="{{ route('users.update', $user->id) }}" method="POST" enctype="multipart/form-data" accept-charset="UTF-8">
           <input type="hidden" name="_method" value="PUT">
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -31,6 +31,14 @@
           <div class="form-group">
             <label for="introduction-field">个人简介</label>
             <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+          </div>
+          <div class="form-group">
+            <label for="avatar_field" class="avatar-label">用户头像</label>
+            <input type="file" name="avatar" id="avatar_field" class="form-control-file">
+
+            @if ($user->avatar)
+                <img src="{{ $user->avatar }}" class="thumbnail img-responsive" width="200" />
+            @endif
           </div>
           <div class="well well-sm">
             <button type="submit" class="btn btn-primary">保存</button>
