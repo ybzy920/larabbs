e817153c88b1749facd77f0d60079dda935a4256
Yangbin <yss920@163.com>
2019-05-13 09:42:29 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
